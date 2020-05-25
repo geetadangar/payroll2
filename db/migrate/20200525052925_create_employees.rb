c84184829bd41ec12a9336861965f82a9945ffb6
geeta <geeta@complitech.net>
2020-05-25 12:59:22 +0530

    commit

---
+++ db/migrate/20200525052925_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[6.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :designation
+      t.string :department
+      t.string :PAN
+      t.string :PF
+
+      t.timestamps
+    end
+  end
+end
