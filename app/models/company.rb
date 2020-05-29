d2f0134d12060b85141125edd9e92573a943b190
Hiral <hiral@complitech.net>
2020-05-29 15:40:33 +0530

    add function for upload excel file.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ApplicationRecord
 	has_many :employees
+	validates_presence_of :name
 end
--- app/models/payroll_import.rb
+++ app/models/payroll_import.rb
@@ -1,70 +1,70 @@
 class PayrollImport < ApplicationRecord
-    include ActiveModel::Model
-    require 'roo'
-  
-    # attr_accessor :file
-  
+  include ActiveModel::Model
+  require 'roo'
+
+    attr_accessor :file
+
     # def initialize(attributes={})
     #   attributes.each { |name, value| send("#{name}=", value) }
     # end
-  
+
     # def persisted?
     #   false
     # end
-  
-    def open_spreadsheet
-      case File.extname(file.original_filename)
-      when ".csv" then Csv.new(file.path, nil, :ignore)
-      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Roo::Excelx.new(file.path)
-      else raise "Unknown file type: #{file.original_filename}"
-      end
-    end
-  
-    def load_imported_salaries
-      spreadsheet = open_spreadsheet
-      header = spreadsheet.row(2)
-      (3..spreadsheet.last_row).map do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-        salary = Salary.new
-        salary.salary_details = row.to_hash
-        salary
-      end
+
+    # def open_spreadsheet
+    #   case File.extname(file.original_filename)
+    #   when ".csv" then Csv.new(file.path, nil, :ignore)
+    #   when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+    #   when ".xlsx" then Roo::Excelx.new(file.path)
+    #   else raise "Unknown file type: #{file.original_filename}"
+    #   end
+    # end
+
+  def open_spreadsheet
+    Roo::Excelx.new('./sample_payroll.xlsx', {:expand_merged_ranges => true})
+  end
+
+  def load_imported_salaries
+    spreadsheet = open_spreadsheet
+    header = spreadsheet.row(2)
+    (3..spreadsheet.last_row).map do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      salary = Salary.new
+      salary.salary_details = row.to_hash
+      salary
+  end
 
       # row = Hash[[header, spreadsheet.row(i)].transpose]
       # #   item = Item.find_by_id(row["id"]) || Item.new
       #   salary << row.to_hash
-        
+
       # end
       # return salary, header
 
     end
-  
-    def imported_salaries
-      @imported_salaries ||= load_imported_salaries
-    end
+
+  def imported_salaries
+    @imported_salaries ||= load_imported_salaries
+  end
 
     # def payroll
     #   @payroll ||= Payroll.new({'company': Company.new, 'for_month': (DateTime.now-1.month).strftime('%B, %Y'), 'salaries': imported_salaries})
     # end
-  
-    def save
-      if imported_salaries.map(&:valid?).all?
-        imported_salaries.each(&:save!)
-        true
-      else
-        imported_salaries.each_with_index do |salary, index|
-          salary.errors.full_messages.each do |msg|
-            errors.add :base, "Row #{index + 6}: #{msg}"
-          end
-        end
-        false
+
+  def save
+    if imported_salaries.map(&:valid?).all?
+      imported_salaries.each(&:save!)
+      true
+    else
+      imported_salaries.each_with_index do |salary, index|
+      salary.errors.full_messages.each do |msg|
+        errors.add :base, "Row #{index + 6}: #{msg}"
       end
     end
-
-
-
-
+      false
+      end
+    end
 
     # def index
     #     # @xlsx = Roo::Spreadsheet.open('./sample_payroll.xlsx')
@@ -80,7 +80,7 @@ class PayrollImport < ApplicationRecord
     #     # => Returns basic info about the spreadsheet file
 
     # end
-    
+
     # def export
     #     kit = PDFKit.new('http://localhost:3000/payslip/show')
     #     file = kit.to_file('./payslip.pdf')
@@ -88,20 +88,16 @@ class PayrollImport < ApplicationRecord
     #     # render :file => file
     # end
 
-    def load_imported_items
-        spreadsheet = open_spreadsheet
-        header = spreadsheet.row(2)
-        salary = []
-        (3..spreadsheet.last_row).map do |i|
-          row = Hash[[header, spreadsheet.row(i)].transpose]
-        #   item = Item.find_by_id(row["id"]) || Item.new
-          salary << row.to_hash
-          
-        end
-        return salary, header
-    end
+    # def load_imported_items
+    #     spreadsheet = open_spreadsheet
+    #     header = spreadsheet.row(2)
+    #     salary = []
+    #     (3..spreadsheet.last_row).map do |i|
+    #       row = Hash[[header, spreadsheet.row(i)].transpose]
+    #     #   item = Item.find_by_id(row["id"]) || Item.new
+    #       salary << row.to_hash
 
-    def open_spreadsheet
-        Roo::Excelx.new('./sample_payroll.xlsx', {:expand_merged_ranges => true})
-    end
+    #     end
+    #     return salary, header
+    # end
 end
